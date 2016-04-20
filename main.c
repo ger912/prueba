538e3472bbe6191fe9017880b3099969e6c4bed6
ger912 <gercoss.92@gmail.com>
2016-04-20 19:52:11 -0300

    socket video1

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: 20/4/2016
+ *      Author: utnso
+ */
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(void) {
+	struct sockaddr_in direcciónServidor;
+	direcciónServidor.sin_family = AF_INET;
+	direcciónServidor.sin_addr.s_addr = INADDR_ANY;
+	direcciónServidor.sin_port = htons(8080);
+
+	int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (bind(servidor, (void*) &direcciónServidor, sizeof(direcciónServidor)) != 0) {
+		perror("Falló el bind");
+		return 1;
+	}
+
+	printf("Estoy escuchando\n");
+	listen(servidor, 100);
+
+	for(;;);
+	return 0;
+}
+
